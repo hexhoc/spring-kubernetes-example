fe930bc823cac7240154336f349f4f3162476299
hexhoc <hexhoc@gmail.com>
2022-05-21 14:21:34 +0300

    Fix docker scripts

--- docker/createDockerImages.bat
+++ docker/createDockerImages.bat
@@ -1,3 +1,6 @@
+@REM before start this script you should do "sudo docker login"
+call docker login
+
 cd "../"
 @REM clean all project builds
 call mvnw.cmd clean -f pom.xml
@@ -5,21 +8,27 @@ call mvnw.cmd clean -f pom.xml
 call mvnw.cmd package -Dmaven.test.skip -f pom.xml
 
 cd "./kbe-coffeehouse-coffee-service"
-call docker build -t example/kbe-coffeehouse-coffee-service .
+call docker build -t hexhoc/kbe-coffeehouse-coffee-service .
 
 cd "../kbe-coffeehouse-gateway"
-call docker build -t example/kbe-coffeehouse-gateway .
+call docker build -t hexhoc/kbe-coffeehouse-gateway .
 
 cd "../kbe-coffeehouse-inventory-failover"
-call docker build -t example/kbe-coffeehouse-inventory-failover .
+call docker build -t hexhoc/kbe-coffeehouse-inventory-failover .
 
 cd "../kbe-coffeehouse-inventory-service"
-call docker build -t example/kbe-coffeehouse-inventory-service .
+call docker build -t hexhoc/kbe-coffeehouse-inventory-service .
 
 cd "../kbe-coffeehouse-order-service"
-call docker build -t example/kbe-coffeehouse-order-service .
+call docker build -t hexhoc/kbe-coffeehouse-order-service .
 
 cd "../docker"
+
+@REM call docker push hexhoc/kbe-coffeehouse-coffee-service
+@REM call docker push hexhoc/kbe-coffeehouse-coffee-service
+@REM call docker push hexhoc/kbe-coffeehouse-inventory-failover 
+@REM call docker push hexhoc/kbe-coffeehouse-inventory-service
+@REM call docker push hexhoc/kbe-coffeehouse-order-service
 
 call docker-compose stop
 call docker-compose rm -f
--- docker/createDockerImages.sh
+++ docker/createDockerImages.sh
@@ -1,3 +1,7 @@
+
+# before start this script you should do "sudo docker login"
+docker login
+
 cd "../"
 # clean all project builds
 sh mvnw clean -f pom.xml
@@ -5,22 +9,28 @@ sh mvnw clean -f pom.xml
 sh mvnw package -Dmaven.test.skip -f pom.xml
 
 cd "./kbe-coffeehouse-coffee-service" || exit
-docker build -t example/kbe-coffeehouse-coffee-service-1.0 .
+docker build -t hexhoc/kbe-coffeehouse-coffee-service .
 
 cd "../kbe-coffeehouse-gateway" || exit
-docker build -t example/kbe-coffeehouse-gateway-1.0 .
+docker build -t hexhoc/kbe-coffeehouse-gateway .
 
 cd "../kbe-coffeehouse-inventory-failover" || exit
-docker build -t example/kbe-coffeehouse-inventory-failover-1.0 .
+docker build -t hexhoc/kbe-coffeehouse-inventory-failover .
 
 cd "../kbe-coffeehouse-inventory-service" || exit
-docker build -t example/kbe-coffeehouse-inventory-service-1.0 .
+docker build -t hexhoc/kbe-coffeehouse-inventory-service .
 
 cd "../kbe-coffeehouse-order-service" || exit
-docker build -t example/kbe-coffeehouse-order-service-1.0 .
+docker build -t hexhoc/kbe-coffeehouse-order-service .
 
 cd "../docker" || exit
 
-docker-compose stop
-docker-compose rm -f
-docker-compose up --build -d
+# docker push hexhoc/kbe-coffeehouse-coffee-service
+# docker push hexhoc/kbe-coffeehouse-coffee-service
+# docker push hexhoc/kbe-coffeehouse-inventory-failover 
+# docker push hexhoc/kbe-coffeehouse-inventory-service
+# docker push hexhoc/kbe-coffeehouse-order-service
+
+# docker-compose stop
+# docker-compose rm -f
+# docker-compose up --build -d
